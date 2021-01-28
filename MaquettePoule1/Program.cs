d19f5763ce25fa20d9a197f221de0c88f040617e
lavancar <71133437+lavancar@users.noreply.github.com>
2021-01-28 13:41:48 +0100

    Initial commit

---
+++ MaquettePoule1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaquettePoule1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
