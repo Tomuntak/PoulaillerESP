79e266f7651a4b17d611b77436110107b844e664
Tomuntak <tomvaldevit38@gmail.com>
2021-01-29 14:15:14 +0100

    Code arduino
    Ceci est le code pour l'ESP8266

---
+++ Projet_v1/Projet_v1.ino
@@ -0,0 +1,54 @@
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WifiClient.h>
+
+#define WIFI_SSID       "linksysTablettes"
+#define WIFI_PASSWORD   "zx2axdt8fmkve3nj"
+WiFiClient client;
+
+bool LastState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  connect_wifi();
+}
+
+
+
+void loop() {
+  
+  BTN_open_close();
+
+
+}
+
+
+
+void BTN_open_close() {
+  if (digitalRead(0) && digitalRead(0) != LastState) { //Fermé
+    LastState = !LastState;
+    Serial.println("Ferme");
+  }
+  else if (digitalRead(0) == 0 && digitalRead(0) != LastState) { //Ouvert
+    LastState = !LastState;
+    Serial.println("Ouvert");
+  }
+  else {
+  }
+  delay(100);
+}
+
+void connect_wifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to the WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
