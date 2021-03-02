ff9d67dc781c5e29c16c8a55523253613ccdaa87
lavancar <71133437+lavancar@users.noreply.github.com>
2021-03-02 11:57:58 +0100

    mise en place de l'écoute
    permet d'écouter le channel MQTT pour l'état de la porte
    Rajout de commentaires

--- PoulaillerMaquette/PoulaillerMaquette/View/uc_home.xaml.cs
+++ PoulaillerMaquette/PoulaillerMaquette/View/uc_home.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +13,67 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace PoulaillerMaquette.View
 {
+    
     /// <summary>
     /// Logique d'interaction pour uc_home
     /// </summary>
     public partial class uc_home : UserControl
     {
+        MqttClient client;
+
+     
+
         public uc_home()
         {
             InitializeComponent();
+            client = new MqttClient(IPAddress.Parse("172.31.253.11"));
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            client.MqttMsgSubscribed += client_MqttMsgSubscribed;
+
+            client.Connect(Guid.NewGuid().ToString());
+            client.Subscribe(new string[] { "etat/porte" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+
         }
+
+        private void BTN_changeporte_Click(object sender, RoutedEventArgs e)
+        {
+ 
+            
+
+            client.Publish("etat/porte", Encoding.UTF8.GetBytes("porte2"));
+        }
+
+
+        //***********************************  fonction de traitement des messages reçus *******************************************************************//
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // access data bytes throug e.Message
+            
+            //ligne a lock en cas de debug **************************************************************************************************
+            string PorteRead = "Topic : " + e.Topic + "Message :" + Encoding.UTF8.GetString(e.Message);
+            //*******************************************************************************************************************************
+
+            if(PorteRead == "ouvert")
+            {
+                TB_sub.Text = "ouvert";
+            }
+            else if(PorteRead == "ferme")
+            {
+                TB_sub.Text = "fermée";
+            }
+
+        }
+
+        void client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+        {
+            
+
+        }
+
     }
 }
